dcde05a4ba221de246ea1e7898cce91decab4926
Juan <jgmunoz@itpfp.com>
2026-05-22 18:46:11 +0200

    Fix exact partidoModel name

--- backend/src/controllers/ligaController.js
+++ backend/src/controllers/ligaController.js
@@ -1,5 +1,5 @@
 const footballService = require("../services/footballService");
-const partidoModel = require("../models/PartidoModel");
+const partidoModel = require("../models/partidoModel");
 
 const obtenerLigas = async (req, res) => {
   try {
