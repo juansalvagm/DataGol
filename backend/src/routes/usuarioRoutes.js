867e290c2fb7ca199770d5db1b333cf5a34ed21e
Juan <jgmunoz@itpfp.com>
2026-05-23 16:42:50 +0200

    Fix rutas usuarios

--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const usuarioController = require("../controllers/usuarioController");
 
 router.get("/", usuarioController.obtenerUsuarios);
+
 router.get("/:id", usuarioController.obtenerUsuarioPorId);
+
 router.post("/", usuarioController.crearUsuario);
+
 router.put("/:id", usuarioController.actualizarUsuario);
+
 router.delete("/:id", usuarioController.eliminarUsuario);
-router.get("/", obtenerUsuarios);
-router.delete("/:id", eliminarUsuario);
 
 module.exports = router;
